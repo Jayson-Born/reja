2d95bc38cf1f9ba834ee4c0ff77b030e73b2f3f5
Jayson-Born <abdukabir34@gmail.com>
2024-11-16 23:51:36 +0900

    feat: develop author route ejs page

--- server.js
+++ server.js
@@ -2,18 +2,36 @@ console.log("Web Serverni boshlash");
 const express = require ("express");
 const app = express ();
 const hhtp = require ("http");
+const fs = require("fs");
 
+let user;
+fs.readFile ("database/user.json", "utf8", (err,data)=>{
+    if (err){
+        console.log("ERROR:", err);
+    }
+})
 // 1
 app.use(express.static("public"));
 app.use(express.json());
 app.use (express.urlencoded({extended :true}));
-//2 Session
+//2 Session 
 
 //3
 app.set("views","views");
 app.set("view engine", "ejs");
 
 //4
+app.post ("/create- item", (req, res)=>{
+    console.log(req.body);
+    res.json({test:"success"});
+})
+app.get ("/author", (req,res) => {
+    res.render("author", {user:user})}
+);
+
+
+
+
 app.get("/", function(req,res){
     res.render("harid")
 });
