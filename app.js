e3b4ad99ddec45297611234b71296c959f30c742
Jayson-Born <abdukabir34@gmail.com>
2024-11-28 18:26:25 +0900

    oxshamayotgan connectionString

--- app.js
+++ app.js
@@ -1,63 +1,67 @@
-console.log("Web Serverni boshlash");
-const express = require ("express");
-const app = express ();
-const fs = require("fs");
+console.log("web Serverni boshlash");
+const express = require("express");
+const app = express();
 
-//MongoDB chaqirish
+// const fs = require("fs");
+
+// let user;
+// fs.readFile("database/user.json", "utf8", (err, data) => {
+//    if(err) {
+//       console.log("ERROR:", err);
+//    } else {
+//       user = JSON.parse(data);
+//    }
+// });
+
 const db = require("./server").db();
 
 
-let user;
-fs.readFile ("database/user.json", "utf8", (err,data)=>{
-    if (err){
-        console.log("ERROR:", err);
-    }
-})
-// 1  kirish
+
+// 1 KIRISH code
 app.use(express.static("public"));
 app.use(express.json());
-app.use (express.urlencoded({extended :true}));
-//2 Session 
+app.use(express.urlencoded({extended: true}));
 
-//3
-app.set("views","views");
+// 2 Session code
+// 3 VIEWS code
+app.set("views", "views");
 app.set("view engine", "ejs");
 
-//4
-app.post ("/create-item", (req, res)=>{
-    console.log(req.body);
-    const new_reja=req.body.reja
-    db.collection("plans").insertOne({reja:new_reja}, (err,data)=>{
-        if(err){
-            console.log(err);
-            res.end("Something went wrong");
-        } else{
-            res.end("successfully added");
-        }
-    });
+
+
+// 4 ROUTING code
+app.post("/create-item", (req,res) => {
+   console.log("user entered /create-item");
+   console.log(req.body);
+   const new_reja = req.body.reja;
+   db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+      if(err) {
+         console.log(err);
+         res.end("Something went wrong");
+      } else {
+         res.end("Succesfully added");
+      }
+   });
+   // TODO: code with db here
+   // console.log(req.body);
 });
-app.get ("/author", (req,res) => {
-  res.render("reja");
+
+// app.get('/author', (req, res) => {
+//    res.render("author", {user: user});
+// });
+
+app.get("/", function (req, res) {
+   console.log("user entered /");
+   db.collection("plans")
+   .find()
+   .toArray((err, data) => {
+      if(err) {
+         console.log(err);
+         res.end("Something went wrong");
+      } else {
+         res.render("reja", {items: data});
+      }
+   });
 });
-
-
-
-
-app.get("/", function (req,res) {
-    console.log(user,entered)
-    db.collection("plans")
-    .find()
-    .toArray((err,data)=>{
-        if(err){
-            console.log(err);
-            res.end("something went wrong");
-        }else{
-            console.log(data);
-            res.render("reja",{items:data});
-        }
-    })
-}
-)
-
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,27 +1,25 @@
-const http =require("http");
-const mongodb = require("mongodb")
-let db;
-const connectionString = 
-"mongodb://atlas-sql-674091e50a29e9546bc16a7a-m9ipv.a.query.mongodb.net/Reja`?ssl=true&authSource=admin"
-mongodb.connect(connectionString,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    },
+const http = require("http");
+const mongodb = require("mongodb");
 
-(err , client) => {
-    if(err) console.log("ERROR on connection MongoDB");
-    else{
-        console.log("MongoDB connection succeed");
+let db;
+const connectionString = "mongodb+srv://Jayson:qasdfghjkl34@jayson.m9ipv.mongodb.net/Reja"
+mongodb.connect(connectionString, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+}, (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+        console.log("MongoDB connecttion succeed");
         module.exports = client;
 
-        const app =require("./app");
+        const app = require("./app");
         const server = http.createServer(app);
-        let PORT= 3000;
-        server.listen(PORT,function (){
-        console.log(`The server is runing successfully on part:${PORT}, http://localhost:${PORT}`);
-    });
-
-    }
-}
-)
+        let PORT = 3000;
+        server.listen(PORT, function () {
+            console.log(
+                `The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`
+            );
+        });
+     }
+  }
+);
